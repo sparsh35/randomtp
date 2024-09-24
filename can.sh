c5d79ca0dbac07a745a361aec8cc410627d62fa7
Sparsh Tewatia <44238245+sparsh35@users.noreply.github.com>
2024-09-24 04:54:56 +0100

    Create can.sh

---
+++ can.sh
@@ -0,0 +1,14 @@
+pip uninstall torch torch-xla -y
+
+# Install PyTorch and PyTorch XLA.
+export DATE="20240828"
+export TORCH_VERSION="2.5.0"
+pip install https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch-${TORCH_VERSION}.dev${DATE}-cp310-cp310-linux_x86_64.whl
+pip install https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch_xla-${TORCH_VERSION}.dev${DATE}-cp310-cp310-linux_x86_64.whl
+
+# Install JAX and Pallas.
+pip install torch_xla[tpu] -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install torch_xla[pallas] -f https://storage.googleapis.com/jax-releases/jax_nightly_releases.html -f https://storage.googleapis.com/jax-releases/jaxlib_nightly_releases.html
+
+# Install other build dependencies.
+pip install -r requirements-tpu.txt
